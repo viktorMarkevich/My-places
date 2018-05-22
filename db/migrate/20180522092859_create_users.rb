06a512e353a5a941a1db19b66c3148f771672050
v-dev <viktor.markevich@faceit.com.ua>
2018-05-22 13:58:32 +0300

    Add trips

--- db/migrate/20180522092859_create_users.rb
+++ db/migrate/20180522092859_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :first_name
-      t.string :laste_name
+      t.string :last_name
       t.string :email
       t.string :password_digest
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_150007) do
+ActiveRecord::Schema.define(version: 2018_05_22_104434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "trips", force: :cascade do |t|
+    t.string "city"
+    t.datetime "date_from"
+    t.datetime "date_to"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "laste_name"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
