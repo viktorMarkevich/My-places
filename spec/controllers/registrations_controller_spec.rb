91263e227eae0255e78e3727bf1773a1caeb11ca
v-dev <viktor.markevich@faceit.com.ua>
2018-05-23 10:56:28 +0300

    Add confirmation sending email

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -15,13 +15,20 @@ describe RegistrationsController, type: :controller do
   # end
 
   context 'when the user is not exist' do
-    it 'responds with a auth_token' do
+    before :each do
       post :create, params: { user: { email: 'fake@email.com',
                                       password: '123456' }}, as: :json
+    end
+
+    it 'responds with a auth_token' do
       expect(json).to eq('status' => 'User created successfully')
       expect(User.count).to eq 1
       expect(User.last.confirmation_token.present?).to eq true
       expect(User.last.confirmation_sent_at.present?).to eq true
+    end
+
+    it 'sends a confirmation email' do
+      expect{ User.last.deliver_confirmation }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 end
