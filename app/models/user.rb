60794ccaca73611be612880b5bae29c71a2f741e
v-dev <viktor.markevich@faceit.com.ua>
2018-05-23 10:44:52 +0300

    Create confirmation token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :confirmation_token
+
+  before_create :generate_confirmation_instructions
+
+  private
+
+    def generate_confirmation_instructions
+      self.confirmation_token = SecureRandom.hex(10)
+      self.confirmation_sent_at = Time.now.utc
+    end
 end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -18,8 +18,10 @@ describe RegistrationsController, type: :controller do
     it 'responds with a auth_token' do
       post :create, params: { user: { email: 'fake@email.com',
                                       password: '123456' }}, as: :json
-      expect(json).to eq("status" => "User created successfully")
+      expect(json).to eq('status' => 'User created successfully')
       expect(User.count).to eq 1
+      expect(User.last.confirmation_token.present?).to eq true
+      expect(User.last.confirmation_sent_at.present?).to eq true
     end
   end
 end
