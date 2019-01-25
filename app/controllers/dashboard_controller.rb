f87a17309adfebb43d77f6e06e97cb2eb68dbe98
Victor <viktor.markevich@faceit.com.ua>
2019-01-25 09:08:00 +0200

    Change json data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
 
   def index
-    render json: { data: 'Hello World from backend!', status: 200 }
+    render json: { data: 'Hello World!', status: 200 }
   end
 end
